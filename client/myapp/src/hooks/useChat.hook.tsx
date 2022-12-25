126a47044c94625742a479d0d6f6765a0f02316f
Farhad74 <evgeniy_mgn_74@mail.ru>
2022-12-25 15:31:29 +0300

    chat fixed

--- client/myapp/src/hooks/useChat.hook.tsx
+++ client/myapp/src/hooks/useChat.hook.tsx
@@ -1,52 +1,34 @@
 import { useEffect, useRef, useState } from 'react'
 import { io } from 'socket.io-client'
 import { MessageModel } from '../models/message.model'
+import { UserModel } from '../models/user.model';
 
 export default function useChat() {
-    const [users2, setUsers] = useState([])
+    const [users, setUsers] = useState([])
     
     const [messages, setMessages] = useState([]);
     const [log, setLog] = useState(null)
-    // const opt = {
-    //     query: {
-    //       roomId: user?.roomId,
-    //       userName: user?.name
-    //     }
-    //   }
     const { current: socket } = useRef(
       io('http://localhost:4000')
     )
-    console.log("on io")
   
     useEffect(() => {
       socket.on('log', (log) => {
         setLog(log)
       })
-
-    //   socket.on('message:add', (data: any) => {
-    //     console.log("message:add use effect", messages, data)
   
-    //   socket.on('user_list:update', (users) => {
-    //     setUsers(users)
-    //   })
+      socket.on('user_list:update', (users: UserModel[]) => {
+        setUsers(users)
+      })
   
       socket.on('message_list:update', (messages) => {
-        console.log("[HOOK] messages", messages);
         setMessages(messages)
       })
     }, [])
   
-    // const sendMessage = (message: MessageModel) => {
-    //   socket.emit('message:send', message)
-    // }
-  
     const removeMessage = (message: MessageModel) => {
       socket.emit('message:remove', message)
     }
-
-    // const testSend = (message: MessageModel) => {
-    //   socket.emit('testEvent', message)
-    // }
   
-    return { users2, messages, log, removeMessage, socket }
+    return { users, messages, log, removeMessage, socket }
   }
